1ea2ab84b6bbd5eb0eb9e7676f1156fc154a477e
wangdong233 <116083316+newcpperrookie@users.noreply.github.com>
2023-02-11 22:57:28 +0800

    Update init.sql
    修改了一些sql建表的语法小错误，现在可以直接执行建表了。

--- pkg/configs/sql/init.sql
+++ pkg/configs/sql/init.sql
@@ -3,13 +3,13 @@ CREATE TABLE `user`
     `id`         bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'PK',
     `name`  varchar(128) NOT NULL DEFAULT '' COMMENT 'UserName',
     `password`   varchar(128) NOT NULL DEFAULT '' COMMENT 'Password',
-    "`title`   varchar(128) NOT NULL DEFAULT '' COMMENT 'Title',"
+    `title`   varchar(128) NOT NULL DEFAULT '' COMMENT 'Title',
     `avatar_url`  varchar(256) NOT NULL DEFAULT '' COMMENT 'AVATAR IMAGE URL',
     `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'User account create time',
     `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'User account update time',
     `deleted_at` timestamp NULL DEFAULT NULL COMMENT 'User account delete time',
     PRIMARY KEY (`id`),
-    KEY          `idx_user_name` (`user_name`) COMMENT 'UserName index'
+    KEY          `idx_user_name` (`name`) COMMENT 'UserName index'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='User account table';
 
 CREATE TABLE `video`
@@ -22,8 +22,8 @@ CREATE TABLE `video`
     `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Video update time',
     `deleted_at` timestamp NULL DEFAULT NULL COMMENT 'Video delete time',
     PRIMARY KEY (`id`),
-    KEY `idx_user_id` (`user_id`) COMMENT 'UserID  index'
-    KEY `idx_create_time` (`create_at` DESC) COMMENT 'CreateTime index'
+    KEY `idx_user_id` (`user_id`) COMMENT 'UserID  index',
+    KEY `idx_create_time` (`created_at` DESC) COMMENT 'CreateTime index'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Video table';
 
 CREATE TABLE `comment`
@@ -34,7 +34,7 @@ CREATE TABLE `comment`
     `content`    TEXT NULL COMMENT 'Content',
     `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Comment create time',
     `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Comment update time',
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
     KEY `idx_video_id` (`video_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Comment table';
 
@@ -45,19 +45,19 @@ CREATE TABLE `follow`
     `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Follow create time',
     `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Follow update time',
     `deleted_at` timestamp NULL DEFAULT NULL COMMENT 'follow delete time',
-    PRIMARY KEY (`follow_id`, `follower_id`)
-    KEY `idx_follow_id` (`follow_id`)
+    PRIMARY KEY (`follow_id`, `follower_id`),
+    KEY `idx_follow_id` (`follow_id`),
     KEY `idx_follower_id` (`follower_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Follow table';
 
-CREATE TABLE `favoriate`
+CREATE TABLE `favoriate_record`
 (
     `video_id`    int(64) NOT NULL DEFAULT 0 COMMENT 'Video ID',
     `user_id`    int(64) NOT NULL DEFAULT 0 COMMENT 'Follower ID',
     `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Favirate create time',
     `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Favirate update time',
-    PRIMARY KEY (`video_id`, `user_id`)
-    KEY `idx_video_id` (`video_id`)
+    PRIMARY KEY (`video_id`, `user_id`),
+    KEY `idx_video_id` (`video_id`),
     KEY `idx_user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Favoriate table';
 
@@ -67,7 +67,7 @@ CREATE TABLE `friend`
     `sec_video_id`    int(64) NOT NULL DEFAULT 0 COMMENT 'Second Friend USER ID',
     `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Friend create time',
     `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Friend update time',
-    PRIMARY KEY (`pri_friend_id`, `sec_friend_id`)
-    KEY `idx_pri_id` (`pri_friend_id`)
-    KEY `idx_sec_id` (`sec_friend_id`)
+    PRIMARY KEY (`pri_friend_id`, `sec_video_id`),
+    KEY `idx_pri_id` (`pri_friend_id`),
+    KEY `idx_sec_id` (`sec_video_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Friend table';
